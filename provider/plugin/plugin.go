65dc15c886e8d2eeea0060f323ddcc94789a5c50
Raffaele Di Fazio <difazio.raffaele@gmail.com>
2022-09-26 19:24:30 +0200

    add code for Records in plugin provider
    Signed-off-by: Raffaele Di Fazio <difazio.raffaele@gmail.com>

--- provider/plugin/plugin.go
+++ provider/plugin/plugin.go
@@ -2,41 +2,57 @@ package plugin
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 
 	"sigs.k8s.io/external-dns/endpoint"
 	"sigs.k8s.io/external-dns/plan"
+	"sigs.k8s.io/external-dns/provider"
 )
 
 type PluginProvider struct {
+	provider.BaseProvider
 	client       *http.Client
 	remoteServer string
 }
 
-func NewPluginProvider() *PluginProvider {
+func NewPluginProvider(url string) *PluginProvider {
 	return &PluginProvider{
-		client: &http.Client{},
+		client:       &http.Client{},
+		remoteServer: url,
 	}
 }
 
 // Records will make a GET call to /records
 func (p PluginProvider) Records(ctx context.Context) ([]*endpoint.Endpoint, error) {
-	p.client.Get(p.)
+	req, err := http.NewRequest("GET", p.remoteServer, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := p.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(string(b))
+
+	endpoints := []*endpoint.Endpoint{}
+	err = json.Unmarshal(b, &endpoints)
+	if err != nil {
+		return nil, err
+	}
+	return endpoints, nil
 }
 
 // ApplyChanges will make a POST to /records
 func (p PluginProvider) ApplyChanges(ctx context.Context, changes *plan.Changes) error {
-
-}
-
-// PropertyValuesEqual makes a GET to /propertyvaluesequal 
-func (p PluginProvider) PropertyValuesEqual(name string, previous string, current string) bool {
-}
-
-// AdjustEndpoints makes a PUT to /endpoints
-func (p PluginProvider) AdjustEndpoints(endpoints []*endpoint.Endpoint) []*endpoint.Endpoint {
-}
-
-// TODO how to map that?
-func (p PluginProvider) GetDomainFilter() endpoint.DomainFilterInterface {
+	return nil
 }
--- provider/plugin/plugin_test.go
+++ provider/plugin/plugin_test.go
@@ -1,7 +1,30 @@
 package plugin
 
-import "testing"
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-func TestRecord(t *testing.T) {
-	// instantiate a fake http server
+	"github.com/stretchr/testify/require"
+	"sigs.k8s.io/external-dns/endpoint"
+)
+
+func TestRecords(t *testing.T) {
+	// instantiate a test http server
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// return a json as response
+		w.Write([]byte(`[{
+			"dnsName" : "test.example.com"
+		}]`))
+	}))
+	defer svr.Close()
+
+	provider := NewPluginProvider(svr.URL)
+	endpoints, err := provider.Records(context.TODO())
+	require.Nil(t, err)
+	require.NotNil(t, endpoints)
+	require.Equal(t, []*endpoint.Endpoint{&endpoint.Endpoint{
+		DNSName: "test.example.com",
+	}}, endpoints)
 }
